af4cf48de5823c7fd777dfb1759a1d42d8655124
Nathan Hietala <Nrhietala@gmail.com>
2021-06-13 20:44:25 -0500

    ContactForm emailing is up and running

--- src/components/Contact/FormInput.js
+++ src/components/Contact/FormInput.js
@@ -2,13 +2,18 @@ import React from "react";
 import { FormInputLabel, FormInput } from "./ContactElements";
 
 const FormTextInput = props => {
-  const { label, type, required, placeholder } = props;
+  const { label, name, type, required, placeholder } = props;
   return (
     <>
       <FormInputLabel htmlFor={label}>
         <span>{label}:</span>
       </FormInputLabel>
-      <FormInput type={type} required={required} placeholder={placeholder} />
+      <FormInput
+        type={type}
+        name={name}
+        required={required}
+        placeholder={placeholder}
+      />
     </>
   );
 };
